f46dbb566ac6b28ecac6204971d1cc2abcffb9b8
Tasneemhasanat97 <tasneem.hasanat01@qmail.cuny.edu>
2023-08-23 00:59:48 -0400

    added sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 const DashboardPage = () => {
   return (
     <div>
       <p>Dashboard Page (Protected)</p>
-      <UserButton afterSignOutUrl="/"/>
     </div> // after sign out reach landing page security feat
   )
 }
