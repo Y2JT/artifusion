38ddddecb0daac798c84f065d71e2fa1bce20764
Tasneemhasanat97 <tasneem.hasanat01@qmail.cuny.edu>
2023-08-24 03:24:25 -0400

    api_limits

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -2,6 +2,9 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import Replicate from "replicate";
 
+import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
+
+
 const replicate = new Replicate({
     auth: process.env.REPLICATE_API_TOKEN!
 });
@@ -21,6 +24,13 @@ export async function POST(
         if (!prompt) {
             return new NextResponse("Prompt are required for computation", { status: 400 });
         }
+
+        const freeTrial = await checkApiLimit();
+
+        if (!freeTrial) {
+            return new NextResponse("Free trial limit reached, please upgrade your account", { status: 403 }); // premium model after
+        }
+
         const response = await replicate.run(
             "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
             {
@@ -29,6 +39,8 @@ export async function POST(
               }
             }
           );
+
+        await increaseApiLimit();
 
         return NextResponse.json(response);
     } catch (error) {
