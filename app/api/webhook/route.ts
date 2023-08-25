da0002a461201a5d3795f8fdfe388b78828cf61c
Tasneemhasanat97 <tasneem.hasanat01@qmail.cuny.edu>
2023-08-25 19:18:42 -0400

    added changes to subscription model

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -50,7 +50,9 @@ export async function POST(req: Request) {
             },
             data: {
                 stripePriceId: subscription.items.data[0].price.id,
-                stripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000),
+                stripeCurrentPeriodEnd: new Date(
+                    subscription.current_period_end * 1000
+                    ),
             },
         });
     }
