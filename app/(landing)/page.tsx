04e49cb9ccad5bedc57f36ebc2f96abcb79ffb58
DoubleU <yuuliu.5533@gmail.com>
2023-08-25 22:05:06 -0400

    CSS file, Background, and Landing Page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,22 +1,34 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import styles from "./landingpage.module.css";
 
 const LandingPage = () => {
     return (
-        <div>
-            Landing Page (Unprotected)
-            <div>
-                <Link href="/sign-in">
-                    <Button>
-                        Login
-                    </Button>
-                </Link>
-                <Link href="/sign-up">
-                    <Button>
-                        Register
-                    </Button>
-                </Link>
+        <div className={styles.landingbox}>
+            <div className={styles.landingpicture}></div>
+            <div className={styles.center}>
+                <div className={styles.fadebox}></div>
+                <div className={styles.fadeinimage}>
+                ArtiFusion
+                </div>
+                <div className={styles.interiorPos}>
+                    <div className={styles.interiorPosA}>
+                    <Link href="/sign-in">
+                        <Button>
+                            Login
+                        </Button>
+                    </Link>
+                    </div>
+                    <div className={styles.interiorPosA}>
+                    <Link href="/sign-up">
+                        <Button>
+                            Register
+                        </Button>
+                    </Link>
+                    </div>
+                </div>
             </div>
+            <div className={styles.centerA}></div>
         </div>
     );
 }
